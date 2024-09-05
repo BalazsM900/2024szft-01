7ebe3464bb514b858d836bcd4d03aa3714f5ad09
BalazsM900 <144907929+BalazsM900@users.noreply.github.com>
2024-09-05 13:56:03 +0200

    Add files via upload

---
+++ Frontend/engine.js
@@ -0,0 +1,9 @@
+
+const allatok=['Busz', 'Vonat', 'Repülő', 'Autó']
+function init() {
+    document.getElementById('body').innerHTML=allatok.map( v=>
+        `<div onclick="f('${ v }')">${ v }</div>`
+    ).join("")
+
+}
+const f = console.log
