d7f153986f8c274f9ce54ce6a13ae23858e47bba
BalazsM900 <144907929+BalazsM900@users.noreply.github.com>
2024-09-06 18:14:26 +0200

    Delete Frontend directory

--- Frontend/engine.js
+++
@@ -1,9 +0,0 @@
-
-const allatok=['Busz', 'Vonat', 'Repülő', 'Autó']
-function init() {
-    document.getElementById('body').innerHTML=allatok.map( v=>
-        `<div onclick="f('${ v }')">${ v }</div>`
-    ).join("")
-
-}
-const f = console.log
